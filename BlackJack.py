f95358cad5036fcbf8978a1a1c02a71b368a00f4
Taner Öztaş <taner@oztas.net>
2020-08-17 17:11:33 +0200

    aufgaben wurden bestimt

--- BlackJack.py
+++ BlackJack.py
@@ -1,10 +1,11 @@
+import random
 print("Welcome to the BlackJack")
 
 """Rules of the game
 1 -Blackjack oyununda amaç 21`i geçmeden krupiyeden daha yüksek bir skoru elde etmektir.
 2- Papaz kız, vale kartlarının sayılasal değeri 10`dur.
 3- As kartının sayısal değeri 1 ya da 11 dir.
-4- Oyunun başında, kurupiye oyunculara ve kendine 2 kart verir. 
+4- Oyunun başında, kurupiye oyunculara ve kendine 2 kart verir.
 5- Oyuncuların kartları açıktır ancak krupiyerin ilk kartı kapalı (Hole kart) ikinci kartı açıktır.
 6- Oyuncunun aldığı ilk iki karttan biri 10 değerinde ve ikinci kart As ise oyuncu Blackjack yapmış olur ve otomatik olarak oyunu kazanır.
 7- Eğer Blackjack durumu söz konusu değilse oyuncular kartlarının sayısal toplamının değerine göre 21`e yaklaşmak için 21`i geçmemek koşulu ile kart istemeye devam edebilirler (hit) ya da oyuncu elinin yeteri kadar güçlü olduğunu düşünüyorsa kart istemeyip durmayı seçer (Stand).
@@ -13,14 +14,33 @@ print("Welcome to the BlackJack")
 10- Krupiyer 21`i geçerse (bust) 21`i geçmeyen bütün oyuncular kazanmış olur.
 11- Oyuncu ve krupiyer aynı skoru elde ederse (Push) oyunda herhangi bir kazanan olmaz.
 
+
+1 - Player ve Dealer'a ikişer kart ver.
+2 - Player'ın iki kartınıda ve yanıda toplamını göster.
+3 - Dealer'ın bir kartını göster diğerini ******* gizle. tek kartın değeerini yaz.
+4 - ilk elde verilen kartları ve çekilen kartları kart havuzundan çıkar.
+
+5 - İlk aşamada BlackJack durumunu kontrol et, varsa bitir.
+    BlackJack durumu ikisinde de varsa, beraberlik.
+6 - Döngü: Oyuncu stand demezse, kart ver.
+           Oyuncu 21'e ulaşırsa döngüden çık makineye kart ver.
+           Oyuncu 21 i geçerse oyunu bitir.
+7 - Stand Derse : Makinenin görünmeyen kartını aç...
+    Makine 17ye eşit yada geçene kadar kart ver.
+8 - Makine 17'ye ulaştığında yada 21'i geçtiğinde oyunu bitir.
+9 - Toplam değerleri kıyasla büyük olanı galip ata.
+10 - Oyunu bitirmek isterse bitir... ve kazanma toplamlarını bas.
+
+
+
 """
 
 
 """Card class that represents a playing card and its image file name."""
-import random
+
 
 class Card:
-    ## constant variable'lar her zaman büyük yazılır.
+    # constant variable'lar her zaman büyük yazılır.
     FACES = ['Ace', '2', '3', '4', '5', '6',
              '7', '8', '9', '10', 'Jack', 'Queen', 'King']
     SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
@@ -68,7 +88,6 @@ class Card:
 """Deck class represents a deck of Cards."""
 
 
-
 class DeckOfCards:
     NUMBER_OF_CARDS = 52  # constant number of Cards
 
@@ -105,5 +124,3 @@ class DeckOfCards:
                 s += '\n'
 
         return s
-
-
