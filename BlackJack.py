2f6301bbb91cf48e47dd19fa21866be73b5674ad
Hasan Emre Tekce <hasan-emre.tekce@stud.hs-hannover.de>
2020-08-17 15:59:46 +0200

    We are working over that project

--- BlackJack.py
+++ BlackJack.py
@@ -1,1 +1,16 @@
 print("Welcome to the BlackJack")
+
+"""Rules of the game
+1 -Blackjack oyununda amaç 21`i geçmeden krupiyeden daha yüksek bir skoru elde etmektir.
+2- Papaz kız, vale kartlarının sayılasal değeri 10`dur.
+3- As kartının sayısal değeri 1 ya da 11 dir.
+4- Oyunun başında, kurupiye oyunculara ve kendine 2 kart verir. 
+5- Oyuncuların kartları açıktır ancak krupiyerin ilk kartı kapalı (Hole kart) ikinci kartı açıktır.
+6- Oyuncunun aldığı ilk iki karttan biri 10 değerinde ve ikinci kart As ise oyuncu Blackjack yapmış olur ve otomatik olarak oyunu kazanır.
+7- Eğer Blackjack durumu söz konusu değilse oyuncular kartlarının sayısal toplamının değerine göre 21`e yaklaşmak için 21`i geçmemek koşulu ile kart istemeye devam edebilirler (hit) ya da oyuncu elinin yeteri kadar güçlü olduğunu düşünüyorsa kart istemeyip durmayı seçer (Stand).
+8- Eğer oyuncu 21`i geçerse oyunu kaybeder. (Bust) bu durumda krupiyerin elinin durumuna bakılmaksızın krupiyer oyunu kazanmış olur.
+9- Bütün oyuncular kart almayı bitirdıkları zaman krupiyer kapalı kartını açar ve elindeki kartların toplam değeri 17`den az ise kart çekmeye devam eder, 17 ve daha büyük ise kart çekmeyi bırakır.
+10- Krupiyer 21`i geçerse (bust) 21`i geçmeyen bütün oyuncular kazanmış olur.
+11- Oyuncu ve krupiyer aynı skoru elde ederse (Push) oyunda herhangi bir kazanan olmaz.
+
+"""
