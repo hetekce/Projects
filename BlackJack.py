7c1b3844ce5d028ad0d8d00eb93efd802dda672b
Hasan Emre Tekce <hasan-emre.tekce@stud.hs-hannover.de>
2020-08-20 14:36:50 +0200

    "Ace" should be fixed and some errors in blackjack conditions, *set_game does not explode anymore

--- BlackJack.py
+++ BlackJack.py
@@ -1,109 +1,5 @@
 import random
 
-"""Rules of the game
-1 -Blackjack oyununda amaç 21`i geçmeden krupiyeden daha yüksek bir skoru elde etmektir.
-2- Papaz kız, vale kartlarının sayılasal değeri 10`dur.
-3- As kartının sayısal değeri 1 ya da 11 dir.
-4- Oyunun başında, kurupiye oyunculara ve kendine 2 kart verir.
-5- Oyuncuların kartları açıktır ancak krupiyerin ilk kartı kapalı (Hole kart) ikinci kartı açıktır.
-6- Oyuncunun aldığı ilk iki karttan biri 10 değerinde ve ikinci kart As ise oyuncu Blackjack yapmış olur ve otomatik olarak oyunu kazanır.
-7- Eğer Blackjack durumu söz konusu değilse oyuncular kartlarının sayısal toplamının değerine göre 21`e yaklaşmak için 21`i geçmemek koşulu ile kart istemeye devam edebilirler (hit) ya da oyuncu elinin yeteri kadar güçlü olduğunu düşünüyorsa kart istemeyip durmayı seçer (Stand).
-8- Eğer oyuncu 21`i geçerse oyunu kaybeder. (Bust) bu durumda krupiyerin elinin durumuna bakılmaksızın krupiyer oyunu kazanmış olur.
-9- Bütün oyuncular kart almayı bitirdıkları zaman krupiyer kapalı kartını açar ve elindeki kartların toplam değeri 17`den az ise kart çekmeye devam eder, 17 ve daha büyük ise kart çekmeyi bırakır.
-10- Krupiyer 21`i geçerse (bust) 21`i geçmeyen bütün oyuncular kazanmış olur.
-11- Oyuncu ve krupiyer aynı skoru elde ederse (Push) oyunda herhangi bir kazanan olmaz.
-
-class Card
-
-    def  -- Kartları Tanımla
-            Face ve Suit objelerini dön....
-            "As" = [1, 11]
-            Repr ve Str fonksiyonlarını olurştur.. f stringle
-
-class CardDecking (Adem)
-
-    
-
-    def -- kart ver ()
-            1 - Random olarak gerekli oyuncuya kart ver.
-    def -- player kart göster
-            2 - Player'ın iki kartınıda ve yanıda toplamını göster. (Sum of CardValues)
-    def -- dealer kart göster ve sakla
-            3 - Dealer'ın bir kartını göster diğerini ******* gizle. tek kartın değeerini yaz.
-    def -- kart çıkar ()
-            4 - ilk elde verilen kartları ve çekilen kartları kart havuzundan çıkar.
-    def -- Status (Böyle bir fonksiyona gerek yok çünkü bunu game class'ı içerisinde blackjack kontrol kısmında tekrardan yapmış bulıunuyoruz.)
-            5 - İlk aşamada BlackJack durumunu kontrol et, varsa bitir.
-                BlackJack durumu ikisinde de varsa, beraberlik.
-
-Class Game
-    def -- BlackJack_Kontrol (Emre)
-           Player ve Dealer' a ikişer kart ver... (CardDecking.kart ver)
-           BlackJack kontrol et. CardValues den çek.. if statemen koy.
-
-    def -- Sum of CardValues(player, dealer) (Emre)
-
-    def -- status Taner
-        6 - Döngü: Oyuncu stand demezse, kart ver. (CardDecking(kart_ver))
-                   Oyuncu 21'e ulaşırsa döngüden çık makineye kart ver.
-                   Oyuncu 21 i geçerse oyunu bitir.
-    def -- stand   Taner
-        7 - Stand Derse : Makinenin görünmeyen kartını aç. return True False
-    def -- DealerTurn Alperen
-        Makine 17ye eşit yada geçene kadar kart ver.
-    def -- Finish_Set: Alperen
-        8 - BlackJack_Kontrol True ise bitir.
-        8 - Makine 17'ye ulaştığında yada 21'i geçtiğinde oyunu bitir.
-        9 - Toplam değerleri kıyasla büyük olanı galip ata.
-
-    def -- Finish_Game Emre
-        10 - Oyunu bitirmek isterse bitir... ve kazanma toplamlarını bas.
-
-"""
-"""
-    def card_giving(self): #gives cards
-        players_faces=[]
-        dealers_faces=[]
-        players_suits=[]
-        dealers_suits=[]
-        
-        total_player = 0
-        total_dealer = 0
-        for i in range(2):
-            player_card_faces=secrets.choice(FACES) #random value from the list FACES
-            player_card_suits=secrets.choice(SUITS)
-
-            dealer_card_faces=secrets.choice(FACES)
-            dealer_card_suits=secrets.choice(SUITS)
-            
-            if CardDecking.player_card_faces >= 2 and CardDecking.player_card_faces <= 10:
-                total_player+=player_card_faces
-            else:
-                total_player+=Values.get(player_card_faces)
-                
-            if dealer_card_faces>=2 and dealer_card_faces<=10:
-                total_dealer+=dealer_card_faces
-            else:
-                total_dealer+=Values.get(dealer_card_faces)
-                
-            
-            players_faces.append(player_card_faces)
-            players_suits.append(player_card_suits)
-            dealers_faces.append(dealer_card_faces)
-            dealers_suits.append(dealer_card_suits)
-            
-        print(f'players cards are {players_faces}, total value of  dealers card is {dealers_faces[0]}')
-        a=[total_player, total_dealer,player_faces, dealer_faces]
-        return a
-
-    def BlackJackControl(self):
-        control=CardDecking.card_giving()
-        if control[0]==21:
-            return True
-        else:
-            return False     
-"""
-
 
 class Card:
     # constants are written always with CAPITAL LETTERS
@@ -254,15 +150,6 @@ class Game:
         else:
             self.finish_set(decks)
             return False
-        #elif self._point_of_dealer == self._point_of_player and self._hand:
-        #   self.finish_set(decks)
-        #   return False
-        # else:
-        #   return False
-
-        # else:
-        #    if self._hand:
-        #        return False
 # ######################## Taner
 
     def __repr__(self):
@@ -379,9 +266,9 @@ class Game:
         self._hand = False
         self._black = False
         self._close = False
-        deck._self = []
+        deck._deck = []
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    g = Game()
 #    g.play()
 
